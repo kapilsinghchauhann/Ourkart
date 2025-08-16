943584c1e5a32aa359a4cfeaee8f43aff2e49ddc
kapilsinghchauhan <kapilc123rajput@gmail.com>
2025-08-16 11:57:32 +0530

    Flipkart Responsive Web Cloning

---
+++ Client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Footer from './Footer.jsx'
+import Header from './Header.jsx'
+
+function App() {
+  return (
+    <div>
+      <Footer/>
+      <Header/>
+
+    </div>
+  )
+}
+
+export default App
